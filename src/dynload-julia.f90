c759a61f0e79f3e447ecdae2867d59b8cdc037c3
Carlos Une <brocolis@eml.cc>
2021-05-06 00:15:17 -0300

    Add jl_float*_types, jl_int*_types, jl_init_fixup()

--- src/dynload-julia.f90
+++ src/dynload-julia.f90
@@ -1,8 +1,8 @@
 module dynload_julia
-use dynload_base, only: dynload_init, dynload_get, dynload_destroy
+use dynload_base, only: dynload_init, dynload_get, dynload_get_pointer, dynload_destroy
 use, intrinsic :: iso_c_binding, only: c_ptr, c_char, c_int, c_null_ptr, c_null_char, c_associated, c_f_procpointer, &
     c_funptr, jl_value_t => c_ptr, jl_sym_t => c_ptr, jl_module_t => c_ptr, &
-    c_float, c_double, c_int8_t, c_int16_t, c_int32_t, c_int64_t
+    c_float, c_double, c_int8_t, c_int16_t, c_int32_t, c_int64_t, jl_datatype_t => c_ptr, c_f_pointer
 
 abstract interface
     subroutine interface_jl_init() bind(c)
@@ -85,26 +85,34 @@ abstract interface
         type(jl_value_t), intent(in), value :: v
         integer(kind=c_int64_t) :: r
     end function
+
 end interface
 
 private :: julia_module_handle, to_c_string
 type(c_ptr) :: julia_module_handle
 
-public :: load_julia, unload_julia, jl_init, jl_eval_string, jl_atexit_hook
-procedure(interface_jl_init), bind(c), pointer :: jl_init => null()
-procedure(interface_jl_eval_string), bind(c), pointer :: real_jl_eval_string => null()
-procedure(interface_jl_atexit_hook), bind(c), pointer :: jl_atexit_hook => null()
-procedure(interface_jl_typeof), bind(c), pointer :: jl_typeof => null()
-procedure(interface_jl_symbol), bind(c), pointer :: jl_symbol => null()
-procedure(interface_jl_new_module), bind(c), pointer :: jl_new_module => null()
-procedure(interface_jl_get_global), bind(c), pointer :: jl_get_global => null()
-procedure(interface_jl_unbox_float32), bind(c), pointer :: jl_unbox_float32 => null()
-procedure(interface_jl_unbox_float64), bind(c), pointer :: jl_unbox_float64 => null()
-procedure(interface_jl_unbox_bool), bind(c), pointer :: jl_unbox_bool => null()
-procedure(interface_jl_unbox_int8), bind(c), pointer :: jl_unbox_int8 => null()
-procedure(interface_jl_unbox_int16), bind(c), pointer :: jl_unbox_int16 => null()
-procedure(interface_jl_unbox_int32), bind(c), pointer :: jl_unbox_int32 => null()
-procedure(interface_jl_unbox_int64), bind(c), pointer :: jl_unbox_int64 => null()
+public :: load_julia, unload_julia
+procedure(interface_jl_init), bind(c), public, pointer :: jl_init => null()
+procedure(interface_jl_eval_string), bind(c), public, pointer :: real_jl_eval_string => null()
+procedure(interface_jl_atexit_hook), bind(c), public, pointer :: jl_atexit_hook => null()
+procedure(interface_jl_typeof), bind(c), public, pointer :: jl_typeof => null()
+procedure(interface_jl_symbol), bind(c), public, pointer :: jl_symbol => null()
+procedure(interface_jl_new_module), bind(c), public, pointer :: jl_new_module => null()
+procedure(interface_jl_get_global), bind(c), public, pointer :: jl_get_global => null()
+procedure(interface_jl_unbox_float32), bind(c), public, pointer :: jl_unbox_float32 => null()
+procedure(interface_jl_unbox_float64), bind(c), public, pointer :: jl_unbox_float64 => null()
+procedure(interface_jl_unbox_bool), bind(c), public, pointer :: jl_unbox_bool => null()
+procedure(interface_jl_unbox_int8), bind(c), public, pointer :: jl_unbox_int8 => null()
+procedure(interface_jl_unbox_int16), bind(c), public, pointer :: jl_unbox_int16 => null()
+procedure(interface_jl_unbox_int32), bind(c), public, pointer :: jl_unbox_int32 => null()
+procedure(interface_jl_unbox_int64), bind(c), public, pointer :: jl_unbox_int64 => null()
+type(jl_datatype_t), public, pointer :: jl_float16_type => null()
+type(jl_datatype_t), public, pointer :: jl_float32_type => null()
+type(jl_datatype_t), public, pointer :: jl_float64_type => null()
+type(jl_datatype_t), public, pointer :: jl_int8_type => null()
+type(jl_datatype_t), public, pointer :: jl_int16_type => null()
+type(jl_datatype_t), public, pointer :: jl_int32_type => null()
+type(jl_datatype_t), public, pointer :: jl_int64_type => null()
 
 contains
     subroutine load_julia(julia_dll, flags, ier)
@@ -131,6 +139,13 @@ contains
         call c_f_procpointer(dynload_get(julia_module_handle, "jl_unbox_int16"//c_null_char), jl_unbox_int16)
         call c_f_procpointer(dynload_get(julia_module_handle, "jl_unbox_int32"//c_null_char), jl_unbox_int32)
         call c_f_procpointer(dynload_get(julia_module_handle, "jl_unbox_int64"//c_null_char), jl_unbox_int64)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_float16_type"//c_null_char), jl_float16_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_float32_type"//c_null_char), jl_float32_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_float64_type"//c_null_char), jl_float64_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_int8_type"//c_null_char), jl_int8_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_int16_type"//c_null_char), jl_int16_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_int32_type"//c_null_char), jl_int32_type)
+        call c_f_pointer(dynload_get_pointer(julia_module_handle, "jl_int64_type"//c_null_char), jl_int64_type)
 
         if (.not. associated(jl_init)) return
         if (.not. associated(real_jl_eval_string)) return
@@ -146,8 +161,33 @@ contains
         if (.not. associated(jl_unbox_int16)) return
         if (.not. associated(jl_unbox_int32)) return
         if (.not. associated(jl_unbox_int64)) return
+        if (.not. associated(jl_float16_type)) return
+        if (.not. associated(jl_float32_type)) return
+        if (.not. associated(jl_float64_type)) return
+        if (.not. associated(jl_int8_type)) return
+        if (.not. associated(jl_int16_type)) return
+        if (.not. associated(jl_int32_type)) return
+        if (.not. associated(jl_int64_type)) return
 
         ier = 0
+    end subroutine
+
+    subroutine jl_init_fixup()
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_float16_type"//c_null_char), c_null_ptr), jl_float16_type)
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_float32_type"//c_null_char), c_null_ptr), jl_float32_type)
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_float64_type"//c_null_char), c_null_ptr), jl_float64_type)
+
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_int8_type"//c_null_char), c_null_ptr), jl_int8_type)
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_int16_type"//c_null_char), c_null_ptr), jl_int16_type)
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_int32_type"//c_null_char), c_null_ptr), jl_int32_type)
+        call c_f_pointer(transfer(&
+            dynload_get_pointer(julia_module_handle, "jl_int64_type"//c_null_char), c_null_ptr), jl_int64_type)
     end subroutine
 
     subroutine unload_julia()
@@ -166,6 +206,13 @@ contains
         jl_unbox_int16 => null()
         jl_unbox_int32 => null()
         jl_unbox_int64 => null()
+        jl_float16_type => null()
+        jl_float32_type => null()
+        jl_float64_type => null()
+        jl_int8_type => null()
+        jl_int16_type => null()
+        jl_int32_type => null()
+        jl_int64_type => null()
 
         call dynload_destroy(julia_module_handle)
     end subroutine
