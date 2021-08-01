a36c2790feadb8e85186966d8a2512271497ed01
Carlos Une <brocolis@eml.cc>
2021-08-01 15:23:17 -0300

    Disable test/call_fortran.f90 on non-Windows systems Issue #5

--- call_fortran.jl
+++ call_fortran.jl
@@ -1,5 +1,7 @@
+if Sys.iswindows()
 # ccall Fortran function "mul(x, y)"
 println(ccall(:mul, Float64, (Float64, Float64), 2.0, 3.0))
 
 # ccall Fortran function "sub(x, y)"
 println(ccall(:sub, Float64, (Float64, Float64), 10.0, 2.0))
+end
